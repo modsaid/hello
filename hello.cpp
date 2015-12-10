e27a7055d6b3f23e33071b010a5de7a252effe84
modsaid <mahmoud@modsaid.com>
2015-12-10 12:17:35 +0200

    Hello world in many languages

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+main(){
+  cout << "Hello you" << endl;	
+  return 0;
+}
---
+++ hello.rb
@@ -0,0 +1,4 @@
+#!/home/mahmoud/.rvm/rubies/ruby-2.2.1/bin/ruby
+
+puts "Hello ruby world"
+
